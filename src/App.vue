f0dc7620141dbdceee8f8155d2aa7151a34aa62d
jramso <josue.rsou2@gmail.com>
2025-02-06 19:50:03 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TutoresModel msg="UAU DOG - Banho e Tosa"/>
+  </div>
+</template>
+
+<script>
+import TutoresModel from './components/TutoresModel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TutoresModel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TutoresModel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    
+    <table>
+      <thead>
+        <tr v-for="tutor in Tutores" v-bind:key="tutor.id">
+          <th>
+            ID:
+          </th>
+          <th>
+            Nome:
+          </th>
+          <th>
+            Telefone:
+          </th>
+          <th>
+            Endereço:
+          </th>
+
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="tutor in Tutores" v-bind:key="tutor.id">
+          <td>
+            {{ Tutor.id }}
+          </td>
+          <td>
+            {{ Tutor.nome }}
+          </td>
+          <td>
+            {{ Tutor.telefone }}
+          </td>
+          <td>
+            {{ Tutor.endereco }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TutoresModel',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
