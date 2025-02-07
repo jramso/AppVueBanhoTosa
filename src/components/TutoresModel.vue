344e99f71a926643d071ef4daa77d05955b8fd65
jramso <josue.rsou2@gmail.com>
2025-02-07 14:48:32 -0300

    listando um tutor, e conectando a api no Render

--- src/components/TutoresModel.vue
+++ src/components/TutoresModel.vue
@@ -4,51 +4,70 @@
     
     <table>
       <thead>
-        <tr v-for="tutor in Tutores" v-bind:key="tutor.id">
-          <th>
-            ID:
-          </th>
-          <th>
-            Nome:
-          </th>
-          <th>
-            Telefone:
-          </th>
-          <th>
-            Endereço:
-          </th>
-
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+          <th>Telefone</th>
+          <th>Endereço</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="tutor in Tutores" v-bind:key="tutor.id">
-          <td>
-            {{ Tutor.id }}
-          </td>
-          <td>
-            {{ Tutor.nome }}
-          </td>
-          <td>
-            {{ Tutor.telefone }}
-          </td>
-          <td>
-            {{ Tutor.endereco }}
-          </td>
+        <tr v-for="tutor in Tutores" :key="tutor.id">
+          <td>{{ tutor.id }}</td>
+          <td>{{ tutor.nome }}</td>
+          <td>{{ tutor.telefone }}</td>
+          <td>{{ tutor.endereco }}</td>
         </tr>
       </tbody>
     </table>
-
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'TutoresModel',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      Tutores: []
+    };
+  },
+  methods: {
+    lista() {
+      axios.get("/Tutor")
+        .then((res) => {
+          console.log(res.data);
+          this.Tutores = res.data; // Corrigido para atualizar o array corretamente
+        })
+        .catch((error) => {
+          console.error("Erro ao buscar tutores:", error);
+        });
+    }
+  },
+  created() {
+    this.lista();
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
 
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #f4f4f4;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    proxy: "https://agendamentobanhoetosa-2.onrender.com"
+  }
+};
